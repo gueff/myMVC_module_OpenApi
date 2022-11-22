89667395be71f74dd26547743b4bc53281d07ffc
gueff <guido.ueffing@web.de>
2022-11-22 08:45:29 +0100

    update

--- Model/Generate.php
+++ Model/Generate.php
@@ -2,23 +2,139 @@
 
 namespace OpenApi\Model;
 
-use cebe\openapi\Reader;
+use MVC\Cache;
 use MVC\Config;
-use MVC\Convert;
-use MVC\DataType\DTArrayObject;
-use MVC\DataType\DTKeyValue;
-use MVC\Debug;
 use MVC\File;
-use MVC\Request;
-use Webbixx\Controller\Api;
+use MVC\Strings;
 
 class Generate
 {
     /**
+     * @example
+     * Generate::classesOnOpenapi3yaml(
+     *      'https://test.api.mediafinanz.de/api/openapi.yaml',
+     *      'API'
+     * );
+     * @param $sOpenApiFile required
+     * @param $sSubDirName required
+     * @param $bUnlinkDir
+     * @return void
+     * @throws \ReflectionException
+     */
+    public static function DTClassesOnOpenapi3yaml($sOpenApiFile = '', $sSubDirName = '', $bUnlinkDir = true)
+    {
+        if (true === empty($sOpenApiFile) || true === empty($sSubDirName))
+        {
+            return false;
+        }
+
+        $sCacheKey = Strings::seofy(__FUNCTION__) . '.' . $sSubDirName . '.' . md5_file($sOpenApiFile);
+        $sDir = File::secureFilePath(Config::get_MVC_MODULE_CURRENT_DATATYPE_DIR() . '/' . $sSubDirName);
+
+        if (false === empty(Cache::getCache($sCacheKey)) && true === file_exists($sDir))
+        {
+            return false;
+        }
+
+        // array of yaml file
+        $aYaml = self::getArrayOfYaml($sOpenApiFile);
+
+        // get schema
+        $aSchema = self::getAllSchemas($aYaml);
+
+        // for openapi version 3 only
+        if (3 !== (int) get($aYaml['openapi']))
+        {
+            return false;
+        }
+
+        // create namespace string
+        $sNamespace = substr(
+            str_replace('/', '\\', str_replace(Config::get_MVC_MODULES_DIR(), '', Config::get_MVC_MODULE_CURRENT_DATATYPE_DIR())
+            . (('' !== $sSubDirName) ? '/' . $sSubDirName : ''))
+            , 1
+        );
+
+        // base setup
+        $aDataType = array(
+            'dir' => $sDir,
+            'unlinkDir' => (boolean) $bUnlinkDir
+        );
+
+        // iterate schema and create classes
+        foreach ($aSchema as $sName => $aValue)
+        {
+            // skip non objects
+            if ('object' !== get($aValue['type']))
+            {
+                continue;
+            }
+
+            // class
+            $aDataType['class'][$sName] = array(
+                'name' => $sName,
+                'file' => $sName . '.php',
+                'namespace' => $sNamespace,
+                'createHelperMethods' => true,
+                'constant' => array(),
+            );
+
+            $aProperty = get($aValue['properties'], array());
+
+            // iterate property array
+            foreach ($aProperty as $sPropertyName => $aPropertySpecs)
+            {
+                $mVar = self::getSchemaItemPropertyType($aPropertySpecs);
+                $mValue = self::getSchemaItemPropertyValue($aPropertySpecs);
+
+                $sRef = get($aPropertySpecs['$ref']);
+                $sItemsRef = get($aPropertySpecs['items']['$ref']);
+
+                // var is type $ref; check type of ref
+                if (null !== $sRef)
+                {
+                    $sNameOfRef = current(array_reverse(explode('/', $sRef)));
+
+                    if ('object' === get($aSchema[$sNameOfRef]['type']))
+                    {
+                        $mVar = '\\' . $aDataType['class'][$sName]['namespace'] . '\\' . $sNameOfRef;
+                        $mValue = "$mVar::create()";
+                    }
+                    elseif ('array' === get($aSchema[$sNameOfRef]['type']))
+                    {
+                        $sSubItemsRef = current(array_reverse(explode('/', get($aSchema[$sNameOfRef]['items']['$ref']))));
+                        $mVar = '\\' . $aDataType['class'][$sName]['namespace'] . '\\' . $sSubItemsRef . '[]';
+                        $mValue = '$this->add_' . $sPropertyName . '(' . '\\' . $aDataType['class'][$sName]['namespace'] . '\\' . $sNameOfRef . '::create());';
+                    }
+                }
+
+                // var is array of type $ref
+                if ($mVar === 'array' && null !== $sItemsRef)
+                {
+                    $sNameOfRef = current(array_reverse(explode('/', $sItemsRef)));
+                    $mVar = '\\' . $aDataType['class'][$sName]['namespace'] . '\\' . $sNameOfRef . '[]';
+                    $mValue = '$this->add_' . $sPropertyName . '(' . '\\' . $aDataType['class'][$sName]['namespace'] . '\\' . $sNameOfRef . '::create());';
+                }
+
+                $aDataType['class'][$sName]['property'][$sPropertyName]['key'] = $sPropertyName;
+                $aDataType['class'][$sName]['property'][$sPropertyName]['var'] = $mVar;
+                $aDataType['class'][$sName]['property'][$sPropertyName]['value'] = $mValue;
+                $aDataType['class'][$sName]['property'][$sPropertyName]['required'] = true;
+                $aDataType['class'][$sName]['property'][$sPropertyName]['forceCasting'] = true;
+            }
+        }
+
+        \MVC\Generator\DataType::create(56)->initConfigArray($aDataType);
+        Cache::saveCache($sCacheKey, $aDataType);
+
+        return true;
+    }
+
+    /**
      * @param $sOpenApiFile
      * @return false|mixed
      */
-    public static function getYaml($sOpenApiFile = '')
+    public static function getArrayOfYaml($sOpenApiFile = '')
     {
         if ('' === $sOpenApiFile)
         {
@@ -33,108 +149,42 @@ class Generate
 
     /**
      * @param array $aYaml
-     * @return mixed|null
+     * @return array
      */
     public static function getAllSchemas(array $aYaml = array())
     {
-        $aSchema = get($aYaml['components']['schemas']);
+        $aSchema = get($aYaml['components']['schemas'], array());
 
         return $aSchema;
     }
 
     /**
-     * @example
-     * Generate::dataTypeClasses(
-     *      'https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/examples/v3.0/petstore.yaml',
-     *      'PET'
-     * );
-     * @param $sOpenApiFile
-     * @param $sSubDirName
-     * @param $bUnlinkDir
-     * @return void
-     * @throws \ReflectionException
+     * @param array $aPropertySpecs
+     * @return mixed|null
      */
-    public static function dataTypeClasses($sOpenApiFile = '', $sSubDirName = '', $bUnlinkDir = true)
+    protected static function getSchemaItemPropertyValue(array $aPropertySpecs = array())
     {
-        $aYaml = self::getYaml($sOpenApiFile);
-        $aSchema = self::getAllSchemas($aYaml);
+        $mValue = get($aPropertySpecs['example']);
+        $mVar = self::getSchemaItemPropertyType($aPropertySpecs);
 
-        $sNamespace = substr(
-            str_replace('/', '\\', str_replace(Config::get_MVC_MODULES_DIR(), '', Config::get_MVC_MODULE_CURRENT_DATATYPE_DIR())
-            . (('' !== $sSubDirName) ? '/' . $sSubDirName : ''))
-            , 1
-        );
-
-        #---------------------------
-
-        // base setup
-        $aDataType = array(
-            'dir' => File::secureFilePath(Config::get_MVC_MODULE_CURRENT_DATATYPE_DIR() . '/' . $sSubDirName),
-            'unlinkDir' => $bUnlinkDir
-        );
-
-        foreach ($aSchema as $sName => $aValue)
+        if (gettype($mValue) != gettype($mVar))
         {
-            $aDataType['class'][$sName] = array(
-                'name' => $sName,
-                'file' => $sName . '.php',
-                'namespace' => $sNamespace,
-                'createHelperMethods' => true,
-                'constant' => array(),
-            );
-
-            $aProperty = get($aValue['properties'], []);
-
-            foreach ($aProperty as $sPropertyName => $aPropertySpecs)
-            {
-                $mVar = get($aPropertySpecs['type']);
-                ('boolean' === $mVar) ? $mVar = 'bool' : false;
-                ('integer' === $mVar) ? $mVar = 'int' : false;
-                $mValue = get($aPropertySpecs['example']);
-
-                if (gettype($mValue) != gettype($mVar))
-                {
-                    $mValue = null;
-//                    $aDataType['class'][$sName]['property'][$sPropertyName]['value'] = $mValue;
-                }
-
-                $sRef = get($aPropertySpecs['$ref']);
-                $sItems = get($aPropertySpecs['items']);
-                $sItemsRef = get($aPropertySpecs['items']['$ref']);
-
-                // var is type $ref
-                if (null !== $sRef)
-                {
-                    $sNameOfRef = current(array_reverse(explode('/', $sRef)));
-                    $mVar = '\\' . $aDataType['class'][$sName]['namespace'] . '\\' . $sNameOfRef;
-                    $mValue = "$mVar::create()";
-//                    $aDataType['class'][$sName]['property'][$sPropertyName]['value'] = $mValue;
-                }
-
-                // var is array of type $ref
-                if ($mVar === 'array' && null !== $sItemsRef)
-                {
-                    $sNameOfRef = current(array_reverse(explode('/', $sItemsRef)));
-                    $mVar = '\\' . $aDataType['class'][$sName]['namespace'] . '\\' . $sNameOfRef . '[]';
-                    $mValue = '$this->add_' . $sPropertyName . '(' . '\\' . $aDataType['class'][$sName]['namespace'] . '\\' . $sNameOfRef . '::create());';
-//                    $aDataType['class'][$sName]['property'][$sPropertyName]['value'] = $mValue;
-                }
-
-                $aDataType['class'][$sName]['property'][$sPropertyName]['key'] = $sPropertyName;
-                $aDataType['class'][$sName]['property'][$sPropertyName]['var'] = $mVar;
-                $aDataType['class'][$sName]['property'][$sPropertyName]['value'] = $mValue;
-                $aDataType['class'][$sName]['property'][$sPropertyName]['required'] = true;
-                $aDataType['class'][$sName]['property'][$sPropertyName]['forceCasting'] = true;
-            }
+            $mValue = null;
         }
 
-//        Debug::display($aDataType);
-
-        \MVC\Generator\DataType::create(56)->initConfigArray($aDataType);
+        return $mValue;
     }
 
-    protected static function getSchemaItem($sItemName = '')
+    /**
+     * @param array $aPropertySpecs
+     * @return mixed|string|null
+     */
+    protected static function getSchemaItemPropertyType(array $aPropertySpecs = array())
     {
+        $mVar = get($aPropertySpecs['type']);
+        ('boolean' === $mVar) ? $mVar = 'bool' : false;
+        ('integer' === $mVar) ? $mVar = 'int' : false;
 
+        return $mVar;
     }
 }
